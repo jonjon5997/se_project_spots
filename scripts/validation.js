ef76fd25b1a506296c4dd3cbcb172677e12a24f2
Jonathan Sanfilippo <jonjon5997@gmail.com>
2024-09-14 12:58:31 -0400

    feat: new validation form

---
+++ scripts/validation.js
@@ -0,0 +1,43 @@
+const showInputError = (formEl, inputElement, errorMsg) => {
+  const errorMsgID = inputElement.id + "-error";
+  const errorMsgEl = formEl.querySelector("#" + errorMsgID);
+  errorMsgEl.textContent = errorMsg;
+  inputElement.classList.add("modal__input_type_error");
+};
+
+const hideInputError = (formEl, inputElement) => {
+  const errorMsgEl = formEl.querySelector(`#${inputElement.id}-error`);
+  errorMsgEl.textContent = "";
+  inputElement.classList.remove("modal__input_type_error");
+};
+
+const checkInputValidity = (formEl, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formEl, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formEl, inputElement);
+  }
+};
+
+const setEventListeners = (formEl) => {
+  const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
+  const buttonElement = formEl.querySelector(".modal__submit");
+
+  // TODO handle initial states
+  //   toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formEl, inputElement);
+      //   toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+const enableValidation = () => {
+  const formList = document.querySelectorAll(".modal__form");
+  formList.forEach((formEl) => {
+    setEventListeners(formEl);
+  });
+};
+
+enableValidation();
